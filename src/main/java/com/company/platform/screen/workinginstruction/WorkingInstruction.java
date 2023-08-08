996b63b58cd35477336fe9cd424afae1b98bd898
DESKTOP-T0FAHQD\Graphic <chenpeisheng@126.com>
2023-08-08 16:20:43 +0800

    新增作业指导书页面，研究页面排版方法

---
+++ src/main/java/com/company/platform/screen/workinginstruction/WorkingInstruction.java
@@ -0,0 +1,10 @@
+package com.company.platform.screen.workinginstruction;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("WorkingInstruction")
+@UiDescriptor("working-instruction.xml")
+public class WorkingInstruction extends Screen {
+}
