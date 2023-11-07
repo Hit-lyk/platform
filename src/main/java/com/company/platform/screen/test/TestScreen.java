2a4dd165aa8545e6268a13c058984b36dfb5cd2d
DESKTOP-T0FAHQD\Graphic <chenpeisheng@126.com>
2023-11-07 16:16:32 +0800

    测试调用python脚本的功能

--- src/main/java/com/company/platform/screen/test/TestScreen.java
+++ src/main/java/com/company/platform/screen/test/TestScreen.java
@@ -1,33 +1,18 @@
 package com.company.platform.screen.test;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.company.platform.entity.Test_sen;
 import io.jmix.core.DataManager;
-import io.jmix.ui.component.ComboBox;
 import io.jmix.ui.component.FileStorageUploadField;
+import io.jmix.ui.component.Label;
 import io.jmix.ui.component.SingleFileUploadField;
-import io.jmix.ui.component.UploadField;
+import io.jmix.ui.component.TextField;
 import io.jmix.ui.screen.Screen;
 import io.jmix.ui.screen.Subscribe;
 import io.jmix.ui.screen.UiController;
 import io.jmix.ui.screen.UiDescriptor;
 import io.jmix.ui.upload.TemporaryStorage;
-import io.jmix.ui.widget.JmixComboBox;
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.*;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.UUID;
-
-import java.sql.Connection;
-import java.sql.*;
-import java.util.UUID;
-
 
 @UiController("TestScreen")
 @UiDescriptor("Test-screen.xml")
@@ -37,104 +22,58 @@ public class TestScreen extends Screen {
     @Autowired
     private TemporaryStorage temporaryStorage;
     @Autowired
-    private DataManager dataManager;
+    private TextField pythonCmd;
+    @Autowired
+    private Label lblResult;
+
 //    @Autowired
 //    private ComboBox<Integer> comboFileNo;
 
     @Subscribe("uploadTest")
     public void onUploadTestFileUploadSucceed(final SingleFileUploadField.FileUploadSucceedEvent event) {
-        UUID id = uploadTest.getFileId();
         File file = temporaryStorage.getFile(uploadTest.getFileId());
-        Connection conn = null;
+        String  strResult="";
+
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hitplatform", "root", "123456");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
+            String strCommand = pythonCmd.getValue()+" output.py";
             InputStream inputStream = new FileInputStream(file);
-            Workbook workBook = new XSSFWorkbook(inputStream);
-            Sheet sheet0 = workBook.getSheetAt(0);
+            OutputStream outputStream = new FileOutputStream("output.py");
 
-            int rownum = sheet0.getPhysicalNumberOfRows();
-            for(int i=1; i<rownum; i++)//第一行为标题
-            {
-                Row row = sheet0.getRow(i);
+            // 创建一个缓冲区
+            byte[] buffer = new byte[1024];
+            int bytesRead;
 
-//                Cell cell = row.getCell(0);
-                Test_sen testEnity = new Test_sen();
-                DataFormatter formatter = new DataFormatter();
-
-                Cell cellSn = row.getCell(1);
-                Cell cellHumidity = row.getCell(4);
-                Cell cellTemp = row.getCell(3);
-                Cell cellRecord_time = row.getCell(2);
-                String cellStrSn = formatter.formatCellValue(cellSn);
-                String cellStrRT = formatter.formatCellValue(cellRecord_time);
-                testEnity.setId(UUID.randomUUID().toString());
-                testEnity.setHumidity((float)cellHumidity.getNumericCellValue());
-                testEnity.setSn(cellStrSn);
-                testEnity.setTemp((float)cellTemp.getNumericCellValue());
-                testEnity.setRecord_time(cellStrRT);
-                dataManager.save(testEnity);
+            // 将文件流中的数据读取到缓冲区中，并写入文件
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
             }
 
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        try {
-            if (conn != null) {
-                conn.close();
+            Process proc=Runtime.getRuntime().exec(strCommand.split(" "));
+            InputStreamReader stdin=new InputStreamReader(proc.getInputStream(), "gbk");//
+            LineNumberReader input=new LineNumberReader(stdin);
+
+            while (true){
+                String printLine = input.readLine();
+                if(printLine==null)
+                    break;
+
+                strResult+=printLine;
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+            if(strResult.length()==0){
+                BufferedReader errorInfo = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+                while (true){
+                    String printLine = errorInfo.readLine();
+                    if(printLine==null)
+                        break;
 
-
-    }
-
-
-
-    public Object getCellFormatValue(Cell cell){
-        Object cellValue = null;
-        if(cell!=null){
-            //判断cell类型
-            switch(cell.getCellType()){
-                case NUMERIC:{
-                    Object inputValue = null;// 单元格值
-                    Long longVal = Math.round(cell.getNumericCellValue());
-                    Double doubleVal = cell.getNumericCellValue();
-                    if(Double.parseDouble(longVal + ".0") == doubleVal){   //判断是否含有小数位.0
-                        inputValue = longVal;
-                    }
-                    else{
-                        inputValue = doubleVal;
-                    }
-                    DecimalFormat df = new DecimalFormat("#.####");    //格式化为四位小数，按自己需求选择；
-                    cellValue= String.valueOf(df.format(inputValue));      //返回String类型
-                    break;
+                    strResult+=printLine;
                 }
-                case FORMULA:{
-                    //判断cell是否为日期格式
-                    if(DateUtil.isCellDateFormatted(cell)){
-                        //转换为日期格式YYYY-mm-dd
-                        cellValue = cell.getDateCellValue();
-                    }else{
-                        //数字
-                        cellValue = String.valueOf(cell.getNumericCellValue());
-                    }
-                    break;
-                }
-                case STRING:{
-                    cellValue = cell.getRichStringCellValue().getString();
-                    break;
-                }
-                default:
-                    cellValue = "";
             }
-        }else{
-            cellValue = "";
+            lblResult.setValue(strResult);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return cellValue;
     }
 }
